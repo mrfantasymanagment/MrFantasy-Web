f30d7d7dc632c91054afb7b60754bdb6cd2f8447
mrfantasymanagment <mrfantasymanagment@gmail.com>
2025-03-19 16:23:02 -0300

    Auto-sync

--- Backend/routes/guia.js
+++ Backend/routes/guia.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { obtenerPlugins } = require('../controllers/guiacontroller');
+const { obtenerPlugins, crearArchivosPlugin } = require('../controllers/guiacontroller');
 
 router.get('/plugins', obtenerPlugins);
+router.post('/crear-archivos', crearArchivosPlugin);
 
 module.exports = router;
+
