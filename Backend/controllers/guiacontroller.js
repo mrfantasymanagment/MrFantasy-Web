92e824c90cc6b8c1bb3511f82e02290b15af5534
mrfantasymanagment <mrfantasymanagment@gmail.com>
2025-03-19 19:00:13 -0300

    Auto-sync

--- Backend/controllers/guiacontroller.js
+++ Backend/controllers/guiacontroller.js
@@ -41,6 +41,11 @@ const agregarPlugin = async (req, res) => {
     );
 
     res.json({ ok: true });
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+}
+};
 };
 
 module.exports = { obtenerPlugins, crearArchivosPlugin, agregarPlugin };
