b3ddd6da18a0111acb33bcba8eebd0b1fa7e62df
mrfantasymanagment <mrfantasymanagment@gmail.com>
2025-03-19 16:22:03 -0300

    Auto-sync

--- Backend/controllers/guiacontroller.js
+++ Backend/controllers/guiacontroller.js
@@ -10,3 +10,28 @@ module.exports = { obtenerPlugins };
 
 //Creacion de Archivos .html, .css y .js
 
+const crearArchivosPlugin = async (req, res) => {
+    const { nombre } = req.body;
+    const TOKEN = process.env.GITHUB_TOKEN;
+    const REPO = 'mrfantasymanagment/MrFantasy-Web';
+    const BASE = `Subpages/Guia/Plugins/${nombre}`;
+    const extensiones = ['html', 'css', 'js'];
+
+    for (const ext of extensiones) {
+        await fetch(`https://api.github.com/repos/${REPO}/contents/${BASE}/${nombre}.${ext}`, {
+            method: 'PUT',
+            headers: {
+                Authorization: `Bearer ${TOKEN}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                message: `add: archivos ${nombre}`,
+                content: Buffer.from('').toString('base64')
+            })
+        });
+    }
+
+    res.json({ ok: true });
+};
+
+module.exports = { obtenerPlugins, crearArchivosPlugin };
