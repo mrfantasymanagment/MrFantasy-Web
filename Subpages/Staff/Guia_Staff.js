99bdacbd0c4ab3a314db32c8d83377811006a3e6
mrfantasymanagment <mrfantasymanagment@gmail.com>
2025-03-19 16:45:14 -0300

    Auto-sync

--- Subpages/Staff/Guia_Staff.js
+++ Subpages/Staff/Guia_Staff.js
@@ -1,96 +1,65 @@
 //Campos Texto
 function limitarPorEspacio(input) {
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    const estilo = window.getComputedStyle(input);
-    ctx.font = `${estilo.fontSize} ${estilo.fontFamily}`;
-    
-    const anchoTexto = ctx.measureText(input.value).width;
-    const anchoDisponible = input.clientWidth - (parseFloat(estilo.paddingLeft) + parseFloat(estilo.paddingRight));
-    
-    if (anchoTexto > anchoDisponible) {
-      input.value = input.value.slice(0, -1);
-    }
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+  const estilo = window.getComputedStyle(input);
+  ctx.font = `${estilo.fontSize} ${estilo.fontFamily}`;
+  
+  const anchoTexto = ctx.measureText(input.value).width;
+  const anchoDisponible = input.clientWidth - (parseFloat(estilo.paddingLeft) + parseFloat(estilo.paddingRight));
+  
+  if (anchoTexto > anchoDisponible) {
+    input.value = input.value.slice(0, -1);
   }
-  
-  document.querySelector('.Texto_Nombre').addEventListener('input', function() {
-    limitarPorEspacio(this);
-  });
-  
-  document.querySelector('.Texto_Descripcion').addEventListener('input', function() {
-    limitarPorEspacio(this);
-  });
-  
-  //Recuperacion Datos Reportes
-  document.getElementById('Añadir_Campo').addEventListener('click', async function() {
-    const nombre = document.getElementById('Nombre').value;
-    const descripcion = document.getElementById('Descripcion').value;
-  
-    if (nombre.trim() === '' || descripcion.trim() === '') {
-        document.getElementById('Reportes_Incompleto').style.display = 'flex';
-        return;
-    }
-  
-    try {
-        const response = await fetch('https://mrfantasy-backend.onrender.com/guia/plugins', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({nombre, descripcion})
-        });
-  
-        const data = await response.json();
-  
-        if (response.ok) {
-            localStorage.setItem('token', data.token);
-            localStorage.setItem('nombre', data.nombre);
-            document.getElementById('Reportes_Agradecimiento').style.display = 'flex';
-            document.getElementById('Nombre').value = '';
-            document.getElementById('Descripcion').value = '';
+}
 
-            //Crear Archivos
-
-            await fetch('https://mrfantasy-backend.onrender.com/guia/crear-archivos', {
-              method: 'POST',
-              headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ nombre })
-          });
-        } else {
-            document.getElementById('Reportes_Incompleto').style.display = 'flex';
-        }
-    } catch (error) {
-        console.log(error);
-        document.getElementById('Reportes_Incompleto').style.display = 'flex';
-    }
-  });
-  
-  
-  document.getElementById('Reportes_Cerrar').addEventListener('click', function() {
-      document.getElementById('Reportes_Agradecimiento').style.display = 'none';
-  });
-  
-  document.getElementById('Reportes_Error_Cerrar').addEventListener('click', function() {
-    document.getElementById('Reportes_Incompleto').style.display = 'none';
-  });
-  
-document.getElementById('Imagen_Input').addEventListener('change', function(e) {
-    const archivo = e.target.files[0];
-    
-    if (archivo) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const imagen = document.getElementById('Imagen_Campo');
-            imagen.style.backgroundImage = `url(${e.target.result})`;
-            imagen.style.backgroundSize = 'cover';
-            imagen.style.backgroundPosition = 'center';
-        };
-        reader.readAsDataURL(archivo);
-    }
+document.querySelector('.Texto_Nombre').addEventListener('input', function() {
+  limitarPorEspacio(this);
 });
 
-//Mostrar Imagen Añadida
+document.querySelector('.Texto_Descripcion').addEventListener('input', function() {
+  limitarPorEspacio(this);
+});
+
+document.getElementById('Añadir_Campo').addEventListener('click', async function() {
+  const nombre = document.getElementById('Nombre').value;
+
+  if (nombre.trim() === '') {
+      document.getElementById('Reportes_Incompleto').style.display = 'flex';
+      return;
+  }
+
+  try {
+      const response = await fetch('https://mrfantasy-backend.onrender.com/guia/crear-archivos', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ nombre })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+          document.getElementById('Reportes_Agradecimiento').style.display = 'flex';
+          document.getElementById('Nombre').value = '';
+      } else {
+          document.getElementById('Reportes_Incompleto').style.display = 'flex';
+      }
+  } catch (error) {
+      console.log(error);
+      document.getElementById('Reportes_Incompleto').style.display = 'flex';
+  }
+});
+
+document.getElementById('Reportes_Cerrar').addEventListener('click', function() {
+  document.getElementById('Reportes_Agradecimiento').style.display = 'none';
+});
+
+document.getElementById('Reportes_Error_Cerrar').addEventListener('click', function() {
+  document.getElementById('Reportes_Incompleto').style.display = 'none';
+});
+
 document.getElementById('Imagen_Input').addEventListener('change', function(e) {
   const archivo = e.target.files[0];
-  
   if (archivo) {
       const reader = new FileReader();
       reader.onload = function(e) {
