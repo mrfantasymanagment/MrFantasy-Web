bff3b8cdb5d262498dab8218a51c28f3bc61139a
mrfantasymanagment <mrfantasymanagment@gmail.com>
2025-03-19 16:33:08 -0300

    Auto-sync

--- Subpages/Staff/Guia_Staff.js
+++ Subpages/Staff/Guia_Staff.js
@@ -32,7 +32,7 @@ function limitarPorEspacio(input) {
     }
   
     try {
-        const response = await fetch('https://mrfantasy-backend.onrender.com/guia/plugins', {
+        const response = await fetch('https://mrfantasy-backend.onrender.com/guia/crear', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({nombre, descripcion})
