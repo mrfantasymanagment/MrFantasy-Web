ce1d6af415b6e965d175517210a735f9c25b0209
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-05 16:17:05 -0300

    Auto-sync

--- Backend/routes/guia.js
+++ Backend/routes/guia.js
@@ -4,4 +4,7 @@ const { agregarSeccion } = require('../controllers/guiacontroller');
 
 router.post('/agregar', agregarSeccion);
 
+const guiaRoutes = require('./routes/guia');
+app.use('/guia', guiaRoutes);
+
 module.exports = router;
