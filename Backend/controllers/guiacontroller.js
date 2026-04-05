3e679492ef0af286e8780d2d388c60c54f3d04b6
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-05 16:40:06 -0300

    Auto-sync

--- Backend/controllers/guiacontroller.js
+++ Backend/controllers/guiacontroller.js
@@ -1,37 +1,50 @@
-const fs = require('fs');
-const path = require('path');
-
-const agregarSeccion = (req, res) => {
+const agregarSeccion = async (req, res) => {
     const { nombre, descripcion, comandos, imagen, enlace } = req.body;
+
+    const TOKEN = process.env.GITHUB_TOKEN;
+    const REPO = 'mrfantasymanagment/MrFantasy-Web';
+    const ARCHIVO = 'Final/Subpages/Guia/Guia.html';
+    const API = `https://api.github.com/repos/${REPO}/contents/${ARCHIVO}`;
+
+    // 1. Leer el archivo actual
+    const response = await fetch(API, {
+        headers: { Authorization: `Bearer ${TOKEN}` }
+    });
+    const data = await response.json();
+
+    // 2. Decodificar, modificar, recodificar
+    let html = Buffer.from(data.content, 'base64').toString('utf8');
 
     const nuevaSeccion = `
     <a href="${enlace}" style="text-decoration: none;">
     <div class="Plugin_Container">
-        <div class="Nombre_Campo">
-            <div class="hproyectos">${nombre}</div>
-        </div>
-        <div class="Descripcion_Campo">
-            <div class="hproyectos2">${descripcion}</div>
-        </div>
-        <div class="Comandos_Campo">
-            <div class="hproyectos2">${comandos}</div>
-        </div>
-        <div class="Imagen_Campo">
-            <img src="${imagen}" class="Instagram_Imagen">
-        </div>
+        <div class="Nombre_Campo"><div class="hproyectos">${nombre}</div></div>
+        <div class="Descripcion_Campo"><div class="hproyectos2">${descripcion}</div></div>
+        <div class="Comandos_Campo"><div class="hproyectos2">${comandos}</div></div>
+        <div class="Imagen_Campo"><img src="${imagen}" class="Instagram_Imagen"></div>
     </div>
     </a>`;
-
-    const rutaHTML = path.join(__dirname, '../../Subpages/Guia/Guia.html');
-    let html = fs.readFileSync(rutaHTML, 'utf8');
 
     html = html.replace(
         '<script src="Guia.js">',
         nuevaSeccion + '\n    <script src="Guia.js">'
     );
 
-    fs.writeFileSync(rutaHTML, html);
+    // 3. Escribir de vuelta a GitHub
+    await fetch(API, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${TOKEN}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: `add: seccion ${nombre}`,
+            content: Buffer.from(html).toString('base64'),
+            sha: data.sha
+        })
+    });
+
     res.json({ ok: true });
 };
 
-module.exports = { agregarSeccion };
+module.exports = { agregarSeccion }
