ab733672d1efe56e63ebd52da0f45b5f578f629a
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-05 22:10:59 -0300

    Auto-sync

--- Backend/controllers/guiacontroller.js
+++ Backend/controllers/guiacontroller.js
@@ -1,7 +1,7 @@
 const db = require('../config/database');
 
 const obtenerPlugins = async (req, res) => {
-    const [Plugins] = await db.query('SELECT * FROM plugins ORDER BY Nombre ASC');
+    const [plugins] = await db.query('SELECT * FROM plugins ORDER BY Nombre ASC');
     res.json(plugins);
 };
 
