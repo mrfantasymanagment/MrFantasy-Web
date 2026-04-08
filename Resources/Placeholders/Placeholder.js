768c7d6d6f7c8a05bbbeac51ac2d78684662ace8
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-07 21:36:27 -0300

    Auto-sync

--- Resources/Placeholders/Placeholder.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { agregarSeccion } = require('../controllers/guiacontroller');
-
-router.post('/agregar', agregarSeccion);
-
-
-module.exports = router;
