4a9d2ab7745c31916eb1ce2daa25193bda294852
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-08 16:37:11 -0300

    Auto-sync

--- Backend/controllers/guiacontroller.js
+++ Backend/controllers/guiacontroller.js
@@ -68,7 +68,32 @@ body{
     }
 }
         `,
-        js: `TEXTO_JS`
+        js: `
+        
+// Seleccionamos todas las imágenes dentro de la clase "imagenes"
+const imagenes = document.querySelectorAll('img');
+
+// Función para pausar todas las animaciones
+function pausarAnimaciones() {
+  imagenes.forEach(imagen => {
+    imagen.style.animationPlayState = 'paused'; // Pausamos la animación
+  });
+}
+
+// Función para reanudar todas las animaciones
+function reanudarAnimaciones() {
+  imagenes.forEach(imagen => {
+    imagen.style.animationPlayState = 'running'; // Reanudamos la animación
+  });
+}
+
+// Cuando el ratón pasa sobre cualquier imagen, pausamos todas las animaciones
+imagenes.forEach(imagen => {
+  imagen.addEventListener('mouseenter', pausarAnimaciones);
+  imagen.addEventListener('mouseleave', reanudarAnimaciones);
+});
+
+        `
     };
 
     for (const ext of extensiones) {
