d4d775b8deddf39ca3dc16dbb2eb743016c1fb14
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-09 14:54:26 -0300

    Auto-sync

--- Subpages/Staff/Guia_Staff.js
+++ Subpages/Staff/Guia_Staff.js
@@ -67,7 +67,9 @@ document.getElementById('Añadir_Campo').addEventListener('click', async functio
 
       if (response.ok) {
           document.getElementById('Reportes_Agradecimiento').style.display = 'flex';
-          document.getElementById('Nombre').value = '';
+          document.querySelectorAll(selectores).forEach(function(el) {
+            el.value = '';
+        });
           imagenBase64 = ''; // ← limpiar después de subir
       } else {
           document.getElementById('Reportes_Incompleto').style.display = 'flex';
