f110a019f7b0a87aed5c858ca3058750c2148e46
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-09 15:10:43 -0300

    Auto-sync

--- Subpages/Staff/Guia_Staff.js
+++ Subpages/Staff/Guia_Staff.js
@@ -58,11 +58,7 @@ document.getElementById('Añadir_Campo').addEventListener('click', async functio
       const data = await response.json();
 
       if (response.ok) {
-        window.addEventListener('load', function() {
-          if (new URLSearchParams(window.location.search).get('exito') === '1') {
-              document.getElementById('Reportes_Agradecimiento').style.display = 'flex';
-          }
-      });
+          window.location.href = 'https://mrfantasymanagment.github.io/MrFantasy-Web/Subpages/Guia/Guia.html';
       } else {
           document.getElementById('Reportes_Incompleto').style.display = 'flex';
       }
