ec9cdfb7bd5e43c56797d4c266a1bab0cb5fda82
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-11 20:05:47 -0300

    Auto-sync

--- Subpages/Guia/Guia.js
+++ Subpages/Guia/Guia.js
@@ -1,29 +1,22 @@
 function crearPlugin(datos) {
     return `
-            <a href="$https://raw.githubusercontent.com/mrfantasymanagment/MrFantasy-Web/refs/heads/main/Resources/Final/BannerMrFantasy.png" style="text-decoration: none;">
-                <div class="Plugin_Container">
-                    <div class="Nombre_Campo">
-                        <div class="hproyectos">TestCss</div>
-                    </div>
-                    <div class="Descripcion_Campo">
-                        <div class="hproyectos2">DescTestCss</div>
-                    </div>
-                    <div class="Comandos_Campo">
-                        <div class="Comando_Campo_1">Test Comando 1</div>
-                        <div class="Comando_Campo_2">Test Comando 2</div>
-                        <div class="Comando_Campo_3">Test Comando 3</div>
-                        <div class="Comando_Campo_4">Test Comando 4</div>
-                        <div class="Comando_Campo_5">Test Comando 5</div>
-                        <div class="Comando_Campo_6">Test Comando 6</div>
-                        <div class="Comando_Campo_7">Test Comando 7</div>
-                        <div class="Comando_Campo_8">Test Comando 8</div>
-                    </div>
-                    <div class="Imagen_Campo">
-                        <img src="https://raw.githubusercontent.com/mrfantasymanagment/MrFantasy-Web/refs/heads/main/Resources/Final/BannerMrFantasy.png" class="Plugin_Imagen">
-                    </div>
-                    <button class="Editar_Plugin_Boton" onclick="event.preventDefault(); editarPlugin('TestCss')">E</button>
-                </div>
-                </a>`;
+    <a href="${datos.Enlace}" style="text-decoration: none;">
+    <div class="Plugin_Container">
+        <div class="Nombre_Campo">
+            <div class="hproyectos">${datos.Nombre}</div>
+        </div>
+        <div class="Descripcion_Campo">
+            <div class="hproyectos2">${datos.Descripcion}</div>
+        </div>
+        <div class="Comandos_Campo">
+            <div class="hproyectos2">${datos.Comando1}</div>
+        </div>
+        <div class="Imagen_Campo">
+            <img src="${datos.Imagen}" class="Plugin_Imagen">
+        </div>
+        <button class="Editar_Plugin_Boton" onclick="event.preventDefault(); editarPlugin('${datos.Nombre}')">E</button>
+    </div>
+    </a>`;
 }
 
 function editarPlugin(nombre) {
