ed479b4fb7ad4f487140b1ab589c55d998700318
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-13 20:24:52 -0300

    Auto-sync

--- Subpages/Staff/Editar/Guia_Edit.js
+++ Subpages/Staff/Editar/Guia_Edit.js
@@ -119,7 +119,8 @@ document.getElementById('Añadir_Campo').addEventListener('click', async functio
         });
 
         if (response.ok) {
-            document.getElementById('Reportes_Agradecimiento').style.display = 'flex';
+            window.location.href = `https://mrfantasymanagment.github.io/MrFantasy-Web/Subpages/Guia/Guia.html?exito=1`;
+        }
         } else {
             document.getElementById('Reportes_Incompleto').style.display = 'flex';
         }
