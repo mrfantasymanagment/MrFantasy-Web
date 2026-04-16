dff4a1d9fe9169e52c9d7b0db911c7afc495a893
mrfantasymanagment <mrfantasymanagment@gmail.com>
2026-04-16 16:48:26 -0300

    Auto-sync

--- Backend/routes/directo.js
+++ Backend/routes/directo.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 const pool = require('../db');
 
 const API_KEY = process.env.YOUTUBE_API_KEY;
